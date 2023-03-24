a07f1a0af77a25aabe2c872c6e37e7012631454f
daniel <daniel.oliveira.au@hotmail.com>
2023-03-24 15:04:27 -0300

    build: main files

---
+++ lib/teste_tecnico_1.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "teste_tecnico_1/version"
+
+module TesteTecnico1
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/teste_tecnico_1_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+RSpec.describe TesteTecnico1 do
+  it "has a version number" do
+    expect(TesteTecnico1::VERSION).not_to be nil
+  end
+
+
+end
